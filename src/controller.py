559fdab783829b9c47a4f29e81e60a4930a35bb7
martin <martin.delahousse@epitech.eu>
2025-03-10 14:33:07 +0100

    [MOD] cors

--- src/controller.py
+++ src/controller.py
@@ -1,8 +1,9 @@
 from flask import Flask, request, jsonify
-
+from flask_cors import CORS
 from src.fizzbuzz import generate_fizzbuzz
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route('/fizzbuzz', methods=['POST'])
